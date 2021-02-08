04f0e6801283a3dd0915318db75461eea5edcda3
b6320502452 <pavich.s@ku.th>
2021-02-08 19:52:25 +0700

    Update lab2.4.c

--- lab2.4.c
+++ lab2.4.c
@@ -11,11 +11,36 @@ int main()
     scanf("%d",&age);
     if(age%2==0)
     {
-
+        if(name>=65 && name<=73)
+        {
+            printf("%s",even_emoji[0]);
+        }
+        else if(name>=74 && name<=82)
+        {
+            printf("%s",even_emoji[1]);
+        }
+        else if(name>=83 && name<=90)
+        {
+            printf("%s",even_emoji[2]);
+        }
     }
     else
     {
-
+        if(age%5==0)
+        {
+            if(name>=65 && name<=73)
+            {
+                printf("%c%s/",(unsigned int)i,odd_emoji[0]);
+            }
+            else if(name>=74 && name<=82)
+            {
+                printf("%c\%s/",(unsigned int)i,odd_emoji[1]);
+            }
+            else if(name>=83 && name<=90)
+            {
+                printf("%c%s/",(unsigned int)i,odd_emoji[2]);
+            }
+        }
 
 
     }
