c0323e02edfff225d0e1ce417f98a6daf6f6bc85
b6320502452 <pavich.s@ku.th>
2021-02-08 18:20:50 +0700

    Update lab2.1.c

--- lab2.1.c
+++ lab2.1.c
@@ -5,5 +5,44 @@ int main()
     char g;
     scanf("%d %d %d",&a,&b,&c);
     sum=a+b+c;
+    if(sum>=80)
+        g='A';
+    else
+    {
+        if(sum>=75)
+            g='B+';
+        else
+        {
+            if(sum>=70)
+                g='B';
+            else
+            {
+                if(sum>=65)
+                    g='C+';
+                else
+                {
+                    if(sum>=60)
+                        g='C';
+                    else
+                    {
+                        if(sum>=55)
+                            g='D+';
+                        else
+                        {
+                            if(sum>=50)
+                                g='D';
+                            else
+                                g='F';
+                        }
 
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+    printf("%c",g);
 }
