922cf88a67107227bb24ce73f19059ae964adf33
b6320502452 <pavich.s@ku.th>
2021-02-08 19:55:09 +0700

    Update lab2.4.c

--- lab2.4.c
+++ lab2.4.c
@@ -1,12 +1,11 @@
-
 #include<stdio.h>
 int main()
 {
     int age;
     int i=92;
     char name;
-    char odd_emoji[10][10]= {"(^_^)","(*o*)","(T_T)"};
-    char even_emoji[10][10]= {"{@_@}","{*v*}","{X_X}"};
+    char odd_emoji[4][10]= {"(^_^)","(*o*)","(T_T)"};
+    char even_emoji[4][10]= {"{@_@}","{*v*}","{X_X}"};
     scanf("%c",&name);
     scanf("%d",&age);
     if(age%2==0)
@@ -41,7 +40,21 @@ int main()
                 printf("%c%s/",(unsigned int)i,odd_emoji[2]);
             }
         }
-
+        else
+        {
+            if(name>=65 && name<=73)
+            {
+                printf("%s",odd_emoji[0]);
+            }
+            else if(name>=74 && name<=82)
+            {
+                printf("%s",odd_emoji[1]);
+            }
+            else if(name>=83 && name<=90)
+            {
+                printf("%s",odd_emoji[2]);
+            }
+        }
 
     }
     return 0;
