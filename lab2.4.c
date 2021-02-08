c2cb7a3ec9e990310c32039e4a4f4a3288269620
b6320502452 <pavich.s@ku.th>
2021-02-08 19:50:53 +0700

    Create lab2.4.c

---
+++ lab2.4.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+int main()
+{
+    int age;
+    int i=92;
+    char name;
+    char odd_emoji[10][10]= {"(^_^)","(*o*)","(T_T)"};
+    char even_emoji[10][10]= {"{@_@}","{*v*}","{X_X}"};
+    scanf("%c",&name);
+    scanf("%d",&age);
+    if(age%2==0)
+    {
+
+    }
+    else
+    {
+
+
+
+    }
+    return 0;
+}
