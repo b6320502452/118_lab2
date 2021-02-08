ee4a81fa8d0441908e093507d3fb669ab3b760a2
b6320502452 <pavich.s@ku.th>
2021-02-08 19:11:23 +0700

    Update lab2.5.c

--- lab2.5.c
+++ lab2.5.c
@@ -5,22 +5,37 @@ int main()
     scanf("%d %d",&a,&b);
     switch(b)
     {
-        case 1 :
-        case 3 :
-        case 5 :
-        case 7 :
-        case 8 :
-        case 10 :
-        case 12 : n = 31;
-                  break;
-        case 2 : n = 28;
-                 break;
-        case 4 :
-        case 6 :
-        case 9 :
-        case 11 : n = 30;
-                  break;
+    case 1 :
+    case 3 :
+    case 5 :
+    case 7 :
+    case 8 :
+    case 10 :
+    case 12 :
+        n = 31;
+        break;
+    case 2 :
+        n = 28;
+        break;
+    case 4 :
+    case 6 :
+    case 9 :
+    case 11 :
+        n = 30;
+        break;
     }
-
+    while(i < n)
+    {
+        i+=7;
+    }
+    i-=7;
+    for(i; i<=n; i++)
+    {
+        a+=1;
+    }
+    if(a>7)
+        a-=7;
+    printf("%d",a);
+    return 0;
 }
 
